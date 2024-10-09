31b6ff05461b7d917a3b231bf5fab8e4a39888c8
Gemcutter <spud.hjc@gmail.com>
2024-10-09 14:31:44 +1100

    added wizard coding

---
+++ Wizard Coding/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#burger").click(function(){
+        toggleMenu()
+    });
+    x = document.cookie;
+    x = x.split(";")
+    cookies = {};
+    for (let i in x) {
+        tmp = x[i].split("=");
+        cookies[tmp[0]] = tmp[1]
+    }
+    if (cookies[menuOpen]=="true") {
+        menuOpen = true
+        document.getElementById("sidebar").style.left = "0"
+    }
+    else {
+        menuOpen = false;
+    }
+    
+});
+
+let menuOpen;
+function toggleMenu() {
+    if (menuOpen) {
+        menuOpen = false;
+        document.cookie = "menuOpen=false";
+        document.getElementById("sidebar").style.left = "-12rem"
+    }
+    else {
+        menuOpen = true;
+        document.cookie = "menuOpen=true";
+        document.getElementById("sidebar").style.left = "0"
+    }
+}
